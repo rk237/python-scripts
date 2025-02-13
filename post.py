2748c446eb69883393c4cc32d85a3cd4e7facd6c
Rahul Kumar Singh <44337011+rk237@users.noreply.github.com>
2025-02-13 12:17:13 +0530

    Update post.py

--- post.py
+++ post.py
@@ -3,6 +3,8 @@ import json
 import os
 
 token = os.environ.get("github_token")
+#below command checks if github token has been exported successfully or not
+#print(f"Token: {token}")
 reponame = input("enter repo name to be created : ")
 url = f"https://api.github.com/"
 headers = {"Authorization": "token {}".format(token)}
@@ -11,4 +13,4 @@ r = requests.post(url+"user/repos", data=json.dumps(data),headers=headers)
 print(r)
 #print(r.json())
 
-print(f"Token: {token}")
+
